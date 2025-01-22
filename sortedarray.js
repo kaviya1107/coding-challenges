2465058eb15092d0a22d4753fa55c36eeab599f0
kaviya1107 <kavithakaviya0326@gmail.com>
2025-01-22 17:47:26 +0530

    add

---
+++ sortedarray.js
@@ -0,0 +1,20 @@
+// Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+// You must write an algorithm with O(log n) runtime complexity.
+// Example 1:
+// Input: nums = [1,3,5,6], target = 5
+// Output: 2
+// Example 2:
+// Input: nums = [1,3,5,6], target = 2
+// Output: 1
+// Example 3:
+// Input: nums = [1,3,5,6], target = 7
+// Output: 4
+function Sorted(val, target) {
+    for (var i = 0; i <= target; i++) {
+        if (target === val[i]) {
+            console.log(i);
+        }
+    }
+}
+console.log(Sorted([1, 3, 5, 6], 5));
+console.log(Sorted([1, 3, 5, 6], 2));
